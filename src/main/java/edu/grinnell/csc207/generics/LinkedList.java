d27efc5cdc687b3f39035ae8b6f96f4c417b1b9f
leria-kosse <leria.kosse@gmail.com>
2026-02-20 10:41:43 -0600

    LInked List generic

---
+++ src/main/java/edu/grinnell/csc207/generics/LinkedList.java
@@ -0,0 +1,52 @@
+package edu.grinnell.csc207.generics;
+
+public class LinkedList<T> {
+     private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node<T> head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.value;
+    }
+
+    public int size() {
+        return size;
+    }
+}
+    
+
